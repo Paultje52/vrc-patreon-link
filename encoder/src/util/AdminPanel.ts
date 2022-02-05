f22ae8aee46bd8cc3073d299d8b2712f79c36890
Paultje52 <paultje52@gmail.com>
2022-02-05 11:35:59 +0100

    Removed temp logs

--- encoder/src/util/AdminPanel.ts
+++ encoder/src/util/AdminPanel.ts
@@ -31,13 +31,6 @@ export default class AdminPanel {
     this.vrChat = vrChat;
     this.database = database;
 
-    setTimeout(async () => {
-      for (let i = 0; i < 10; i++) {
-        await new Promise((res) => setTimeout(res, 2000));
-        this.onLog("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
-      }
-    }, 5000);
-
     logger.onLog(this.onLog.bind(this));
     this.registerEvents();
   }
